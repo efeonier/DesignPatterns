a08fdc8f026556917ebc2fd425667439233afccd
efe.onier <efeonier@outlook.com>
2024-07-07 16:27:13 +0300

    observer pattern

---
+++ WebApp.ObserverPattern/Context/AppIdentityDbContext.cs
@@ -0,0 +1,12 @@
+﻿using WebApp.ObserverPattern.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApp.ObserverPattern.Context;
+
+public class AppIdentityDbContext : IdentityDbContext<AppUser>
+{
+    public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : base(options)
+    {
+    }
+}
---
+++ WebApp.ObserverPattern/Entities/AppUser.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebApp.ObserverPattern.Entities;
+
+public class AppUser : IdentityUser
+{
+}
